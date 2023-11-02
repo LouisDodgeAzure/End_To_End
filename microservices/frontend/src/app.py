aeab2be2c933c15a8a975f5dcef18db2a02d8d25
Louis Dodge <louis.dodge@displaydata.com>
2023-11-02 15:17:56 +0000

    Initial containerisation

--- microservices/frontend/src/app.py
+++ microservices/frontend/src/app.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+
+# Load configuration from environment variables
+app.config['DEBUG'] = os.environ.get('FLASK_DEBUG', False)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
