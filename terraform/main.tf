310b0b449b2a44689c7c52408cf6d539c2fbfee4
Louis Dodge <louis.dodge@displaydata.com>
2023-11-03 22:57:39 +0000

    Initial Terraform commit

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "main" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_service_plan" "main" {
+  name                = "${var.webapp_name}-asp"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "main" {
+  name                = var.webapp_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  service_plan_id     = azurerm_service_plan.main.id
+
+  site_config {
+    always_on        = true
+
+    application_stack {
+      docker_image_name = "${azurerm_container_registry.main.name}.azurecr.io/${var.image_name}"
+      docker_registry_url = "https://${azurerm_container_registry.main.login_server}"
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.main.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_linux_web_app.main.identity[0].principal_id
+}
