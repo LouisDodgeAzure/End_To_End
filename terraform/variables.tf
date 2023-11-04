ff5e3768592e90165afc70c4aad2855bf1e931b0
Louis <louisdodge2000@gmail.com>
2023-11-04 21:45:44 +0000

    Normalized line endings

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,29 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   default     = "ldendtoend"
+  type = string
 }
 
 variable "location" {
   description = "Azure region for the resources"
   default     = "UK South"
+  type = string
 }
 
 variable "acr_name" {
   description = "Name of the Azure Container Registry"
   default     = "ldacrendtoend"
+  type = string
 }
 
 variable "image_name" {
     description = "Name and version of the webapp image e.g. myimage:latest"
     default = "myimage:latest"
+    type = string
 }
 
 variable "webapp_name" {
   description = "Name for the Azure Web App"
   default     = "LdFlaskWebApp"
+  type = string
 }
