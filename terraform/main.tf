65df9bcb4b6366494cf0da76975f0fb29854839d
Louis <louisdodge2000@gmail.com>
2023-11-19 18:51:55 +0000

    Test revert to azuread_auth for backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
     storage_account_name = "tfbackendwebappsa"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_oidc             = true
+    use_azuread_auth = true
   }
 }
 
