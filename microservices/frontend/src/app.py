7df5223d5c204cfe68bab32c0e7165e42869717a
Louis <louisdodge2000@gmail.com>
2023-11-19 18:02:41 +0000

    Add initial deploy pipeline

--- microservices/frontend/src/app.py
+++ microservices/frontend/src/app.py
@@ -8,7 +8,7 @@ app.config['DEBUG'] = os.environ.get('FLASK_DEBUG', False)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! This image has been updated.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
